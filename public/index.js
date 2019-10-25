509c87c6aaff01e8121d8e849d4d6e969cd129af
CN72 <cn72@CN72.local>
2019-10-25 10:23:45 +0100

    made fetch urls relative and changed port 3000 to port variable

--- public/index.js
+++ public/index.js
@@ -1,76 +1,66 @@
-console.log('up and running...')
+console.log("up and running...");
 
 // Setup DOM methods to HTML
-const inputNews = document.getElementById('inputNews')
-const getNews = document.getElementById('getNews')
-const loading = document.getElementById('loading')
-const newsWrapper = document.getElementById('newsWrapper')
-
-
+const inputNews = document.getElementById("inputNews");
+const getNews = document.getElementById("getNews");
+const loading = document.getElementById("loading");
+const newsWrapper = document.getElementById("newsWrapper");
 
 // when the 'request for news' button being clicked
 // send a get request to the endpoint on localhost:3000/news
-// 
+//
 //      Hence for a location of 'Chester' and category 'business'
 //
 //      http:/localhost:3000/news?category=business
 //
-//      
+//
 
+getNews.addEventListener("click", async () => {
+  loading.textContent = "getting the news...";
 
+  let response = await fetch(`/news?category=${inputNews.value}`);
+  let data = await response.json();
 
-getNews.addEventListener('click', async () => {
+  // let data = testData  // TEMP!!!
+  console.log(" we should have returned data as an array containing 5 objects");
+  console.table(data);
 
-    
-    loading.textContent = "getting the news..."
-    
-    let response = await fetch(`http://localhost:3000/news?category=${inputNews.value}`)
-    let data = await response.json()
+  loading.textContent = "";
+  newsWrapper.textContent = "";
 
-    // let data = testData  // TEMP!!!
-    console.log(' we should have returned data as an array containing 5 objects')
-    console.table(data)
+  // create elements inside the DOM newsWrapper
+  for (article = 0; article < data.length; article++) {
+    console.log(`article number  ${article}`);
+    let newsCard = document.createElement("div");
+    newsCard.innerHTML = "";
+    newsWrapper.appendChild(newsCard);
+    newsCard.setAttribute("class", "news-card");
+    let textwrapper = document.createElement("div");
+    textwrapper.innerHTML = "";
+    newsCard.appendChild(textwrapper);
+    textwrapper.setAttribute("class", "textwrapper");
+    let titleNews = document.createElement("h2");
+    titleNews.textContent = data[article].title;
+    textwrapper.appendChild(titleNews);
+    titleNews.setAttribute("class", "titleNews");
+    let descriptionNews = document.createElement("p");
+    descriptionNews.textContent = data[article].description;
+    textwrapper.appendChild(descriptionNews);
+    descriptionNews.setAttribute("class", "descriptionNews");
+    let linkNews = document.createElement("a");
+    linkNews.textContent = "LINK";
+    linkNews.setAttribute("href", data[article].url);
+    textwrapper.appendChild(linkNews);
+    linkNews.setAttribute("class", "linkNews");
+    let imgNews = document.createElement("a");
+    newsCard.appendChild(imgNews);
+    imgNews.setAttribute("class", "imgNews");
+    imgNews.setAttribute("href", data[article].url);
+    let img = document.createElement("img");
+    img.innerHTML = "";
+    imgNews.appendChild(img);
+    img.setAttribute("src", data[article].urlToImage);
+  }
 
-    loading.textContent = ""
-    newsWrapper.textContent = ""
-
-    // create elements inside the DOM newsWrapper
-    for(article=0; article<data.length; article++){
-        console.log(`article number  ${article}`)
-        let newsCard = document.createElement('div')
-        newsCard.innerHTML = ""
-        newsWrapper.appendChild(newsCard)
-        newsCard.setAttribute("class", "news-card")
-        let textwrapper = document.createElement('div')
-        textwrapper.innerHTML = ""
-        newsCard.appendChild(textwrapper)
-        textwrapper.setAttribute("class", "textwrapper")
-        let titleNews = document.createElement('h2')
-        titleNews.textContent = data[article].title
-        textwrapper.appendChild(titleNews)
-        titleNews.setAttribute("class", "titleNews")
-        let descriptionNews = document.createElement('p')
-        descriptionNews.textContent = data[article].description
-        textwrapper.appendChild(descriptionNews)
-        descriptionNews.setAttribute("class", "descriptionNews")
-        let linkNews = document.createElement('a')
-        linkNews.textContent = 'LINK'
-        linkNews.setAttribute("href", data[article].url)
-        textwrapper.appendChild(linkNews)
-        linkNews.setAttribute("class", "linkNews")
-        let imgNews = document.createElement('a')
-        newsCard.appendChild(imgNews)
-        imgNews.setAttribute("class", "imgNews")
-        imgNews.setAttribute("href", data[article].url)
-        let img = document.createElement('img')
-        img.innerHTML = ''
-        imgNews.appendChild(img)
-        img.setAttribute("src", data[article].urlToImage)
-    }
-
-
-    
-    //needs a catch in case our server is down
-})
-
-
+  //needs a catch in case our server is down
+});
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-const express = require('express')
-const path = require('path')
-const {main} = require('./lib/app')
+const express = require("express");
+const path = require("path");
+const { main } = require("./lib/app");
 
-const app = express()
+const port = process.env.PORT || 3000;
+
+const app = express();
 //this serves our static library up on our front end and provides the absolute path to do so
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
 
 //our main function will take two parameters location and category from input forms on client side.
-app.get('/news', async(req, res)=>{
-    let data = await main(req.query.category)
-    res.send(data)
-})
+app.get("/news", async (req, res) => {
+  let data = await main(req.query.category);
+  res.send(data);
+});
 
-app.listen(3000, ()=>{
-    console.log('listening on port 3000')
-})
+app.listen(port, () => {
+  console.log("listening on port 3000");
+});
